3fe98361e5bbdfddfa1999a110b45e6141dc17e5
elinnaga <einoexequiel.linnagarzon@everis.nttdata.com>
2022-01-10 17:52:00 -0300

    Visuales

---
+++ Tic Tac Toe G3/TicTacToe/GUI/Views/LobbyView.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GUI.Views
+{
+    /// <summary>
+    /// Lógica de interacción para Window1.xaml
+    /// </summary>
+    public partial class LobbyView : Window
+    {
+        private TicTacToeView ticTacToe;
+        public LobbyView()
+        {
+            InitializeComponent();
+
+            ticTacToe = new TicTacToeView();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Start(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            this.Hide();
+            ticTacToe.Show();
+
+
+        }
+    }
+}
